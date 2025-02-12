27de9e6a4b0282260556b620a5f12f58aea0c14a
Thomas-Zayas <tmzayas@miner.utep.edu>
2025-02-12 11:41:31 -0700

    comment

--- draw.h
+++ draw.h
@@ -2,9 +2,11 @@
 #define DRAW_INCLUDED
 
 void print_triangle(int startCol, int size);
-void print_square(int startCol, int size);
+void print_square(int leftCol, int size);
 void print_char_5x7(char c);
+void print_arrow(int leftCol, int size);
 
 extern const unsigned char font_5x7[][5];
+extern const unsigned char font_8x12[][12]
 
 #endif
